0499bcb9cb7edebea44aa0d5ff84dfca6bc6a8f7
Navian <primeguyy@gmail.com>
2021-10-23 22:39:52 +0000

    map method

--- ABitOnStreams.java
+++ ABitOnStreams.java
@@ -1,13 +1,23 @@
 package functionalInterfaces;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ABitOnStreams {
 
     public static void main(String[] args) {
         List<String> names = List.of("Jones", "Pete", "Mariam", "Anne", "Charles");
 
-        names.stream().forEach(name -> System.out.println(name));
+        // map method
+        List<String> nameRecord = names.stream()
+                                        .map(name -> name + " (human)")
+                                        .collect(Collectors.toList());
+
+        System.out.println(nameRecord);
+
+        // for each method
+//        names.stream().forEach(name -> System.out.println(name));
+        names.stream().forEach(System.out::println);
     }
 
 }
