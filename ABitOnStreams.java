d86d956db865335483f36e4c038b893d41ff9609
Navian <primeguyy@gmail.com>
2021-10-23 22:50:52 +0000

    any match and all match methods

--- ABitOnStreams.java
+++ ABitOnStreams.java
@@ -13,11 +13,21 @@ public class ABitOnStreams {
                                         .map(name -> name + " (human)")
                                         .collect(Collectors.toList());
 
+        // any match method
+        boolean findAnyMatch = names.stream().anyMatch(name -> name.equals("Pete"));
+
+        // all match method
+        boolean findAllMatch = names.stream().allMatch(name -> name.equals("Pete"));
+
         System.out.println(nameRecord);
+        System.out.println("--------------");
+        System.out.println(findAnyMatch);
+        System.out.println(findAllMatch);
+        System.out.println("--------------");
 
         // for each method
 //        names.stream().forEach(name -> System.out.println(name));
-        names.stream().forEach(System.out::println);
+        names.forEach(System.out::println);
     }
 
 }
